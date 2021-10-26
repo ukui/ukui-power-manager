ccd100844bb05b9f70689b9a542269a0f01f11bc
handsome-feng <jianfengli@ubuntukylin.com>
2021-10-26 16:35:08 +0800

    copyright header: LGPL-2.1 -> GPL-3+; Add COPYING file

--- ukui-power-manager-tray/enginedevice.cpp
+++ ukui-power-manager-tray/enginedevice.cpp
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2019 Tianjin KYLIN Information Technology Co., Ltd.
+ * Copyright 2021 KylinSoft Co., Ltd.
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,9 +12,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/&gt;.
- *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include "enginedevice.h"
 #include "device.h"
 #include <QDBusObjectPath>
